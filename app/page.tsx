8777d74fcde0afcbd246dafaceb0f426690834ff
Zdenko Markovic <113395395+zdenkomarkovic@users.noreply.github.com>
2025-08-23 19:47:56 +0200

    Updated page.tsx

--- app/page.tsx
+++ app/page.tsx
@@ -2,7 +2,7 @@ import About from "@/components/About";
 import BackgroundImage from "@/components/BackgroundImage";
 import Cardlist from "@/components/Cardlist";
 import Hero from "@/components/Hero";
-import PozoviteNasOdmah from "@/components/PozoviteNasOdmah";
+
 import Statistika from "@/components/Statistika";
 import Testimonials from "@/components/Testimonials";
 
